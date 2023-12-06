c64bc94c778dabbe32049d55e30090bfd8aadf36
Adrian M <adrian.madejski@vp.pl>
2023-12-06 18:57:48 +0100

    Implement DELETE command

--- untitled/src/main/java/org/example/WarehouseManagementApp.java
+++ untitled/src/main/java/org/example/WarehouseManagementApp.java
@@ -15,7 +15,8 @@ public class WarehouseManagementApp {
         while (true) {
             System.out.println("Wybierz akcję:");
             System.out.println("1. Dodaj produkt");
-            System.out.println("2. Wyjście");
+            System.out.println("2. Usuń produkt");
+            System.out.println("3. Wyjście");
 
             int choice = scanner.nextInt();
 
@@ -24,6 +25,9 @@ public class WarehouseManagementApp {
                     addProduct(scanner);
                     break;
                 case 2:
+                    deleteProduct(scanner);
+                    break;
+                case 3:
                     System.out.println("Dziękujemy. Do widzenia!");
                     System.exit(0);
                 default:
@@ -45,6 +49,24 @@ public class WarehouseManagementApp {
 
         System.out.println("Utworzono produkt o identyfikatorze: " + productId);
     }
+    private static void deleteProduct(Scanner scanner) {
+        System.out.println("Podaj identyfikator produktu do usunięcia:");
+        String idString = scanner.next();
+
+        try {
+            UUID productId = UUID.fromString(idString);
+
+            if (products.containsKey(productId)) {
+                products.remove(productId);
+                System.out.println("Usunięto produkt.");
+            } else {
+                System.out.println("Brak produktu o zadanym identyfikatorze.");
+            }
+        } catch (IllegalArgumentException e) {
+            System.out.println("Nieprawidłowy format identyfikatora. Spróbuj ponownie.");
+        }
+    }
+}
 }
 
 class Product {
