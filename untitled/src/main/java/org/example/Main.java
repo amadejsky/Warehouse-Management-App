bdb4357c1d31773a7067099f90e04237b322bfa6
Adrian M <adrian.madejski@vp.pl>
2023-12-06 18:49:33 +0100

    second commit

--- untitled/src/main/java/org/example/Main.java
+++ untitled/src/main/java/org/example/Main.java
@@ -1,7 +1,7 @@
-package org.example;
+package warehouse.management.app;
 
-public class Main {
+public class App {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // ...
     }
 }
